b6c62ac87836b0623d64153b98505989a05f44a7
Mikael Karlsson <mikael.karlsson@creado.se>
2023-03-11 16:52:27 +0100

    Fix guard in skolan_utils:weighted_pup_per_teacher/2

--- src/skolan_utils.erl
+++ src/skolan_utils.erl
@@ -11,12 +11,14 @@ weighted_pup_per_teacher(PupilsPerTeacher, <<"-">>)
   when is_binary(PupilsPerTeacher) ->
     weighted_pup_per_teacher(bstring_to_float(PupilsPerTeacher), 0.0);
 weighted_pup_per_teacher(PupilsPerTeacher, CertifiedTeachersQuota) when
-      is_binary(PupilsPerTeacher); is_binary(CertifiedTeachersQuota) ->
+      is_binary(PupilsPerTeacher), is_binary(CertifiedTeachersQuota) ->
     weighted_pup_per_teacher(bstring_to_float(PupilsPerTeacher),
                              bstring_to_float(CertifiedTeachersQuota));
 weighted_pup_per_teacher(PupilsPerTeacher, CertifiedTeachersQuota) when
-      is_number(PupilsPerTeacher); is_number(CertifiedTeachersQuota) ->
-    100*PupilsPerTeacher/(50.0 + CertifiedTeachersQuota*0.5).
+      is_number(PupilsPerTeacher), is_number(CertifiedTeachersQuota) ->
+    100*PupilsPerTeacher/(50.0 + CertifiedTeachersQuota*0.5);
+weighted_pup_per_teacher(_, _) ->
+    <<"-">>.
 
 average(gy) -> weighted_pup_per_teacher(12.3, 83.5);
 average(gr) -> weighted_pup_per_teacher(12.2, 71.0).
